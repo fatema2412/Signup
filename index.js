777e42223b729c6b35c7c78fbc2f00201d144345
fatema2412 <abbas.ghaniwala@smarthub.ai>
2024-11-28 21:19:45 +0530

    login

--- index.js
+++ index.js
@@ -19,7 +19,52 @@ else{
 }
 }
 
-console.log(signup("fatema"));
-console.log(signup("John"));
-console.log(signup("fatema"))
+// console.log(signup("fatema"));
+// console.log(signup("John"));
+// console.log(signup("fatema"))
+
+function login(userName,password){
+    let found_userName=0;
+    let found_password=0;
+    let arr=["aaditi","John","alice","grecce","zaveri","bohn","kon"];
+for (let i=0;i<arr.length;i++){
+    if(userName==arr[i] && password=="Emp@123"){
+        found_userName++
+        found_password++
+    }
+    else if (userName==arr[i] && password !="Emp@123"){
+        found_userName++
+    }
+}
+if(found_userName>0 && found_password>0 ){
+    return "Login Sucessfull"
+
+}
+else if(found_userName>0){
+    return "Wrong Password...."
+}
+else {
+    return "User Not Found, Please Signup"
+}
+}
+
+console.log(login("John","Emp@123"))
+console.log(login("fatema","Emp@123"))
+console.log(login("zaveri",1234));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
